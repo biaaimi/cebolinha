9727664ff92f30de44aeeb789c5574eb78bd2a12
biaaimi <biagioni.aimi@gmail.com>
2021-09-03 23:18:30 -0300

    cebolinha

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write(" cite uma frase para o cebolinha dizer");
+            string texto =Console.ReadLine( );
+
+
+            Console.ForegroundColor= ConsoleColor.DarkRed;
+            Console.Write("Cebolinha");
+
+            Console.ResetColor( );
+            Console.WriteLine( texto
+            .Replace("r", "l")
+            .Replace("R", "L") );
+
+            Console.ResetColor();
+
+        }
+    }
+}
